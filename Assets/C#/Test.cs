ddf18232925a866362ec6a502700f31c104f9080
aod5906 <aod5906@naver.com>
2024-02-02 18:49:50 +0900

    테스트용

---
+++ Assets/C#/Test.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Hello,World를 Console 창에 출력한다.
+        // Debug.Log("Hello.World");
+
+        // int age;
+        // age = 30;
+        // Debug.Log(age);
+
+        // float height1 = 160.5f;
+        // float height2;
+        // height2 = height1;
+        // Debug.Log(height2);
+
+        // string name;
+        // name = "sera";
+        // Debug.Log(name);
+
+        //int answer;
+        // answer = 1 + 2;
+        // Debug.Log(answer);
+
+        //int answer;
+        // answer = 9 - 3;
+        // Debug.Log(answer);
+
+        //  answer = 4 * 3;
+        //   Debug.Log(answer);
+
+        //  answer = 10 / 2;
+        //   Debug.Log(answer);
+
+        //int n1 = 9;
+        //int n2 = 4;
+        //int answer;
+        //answer = n2 *n1;
+        //Debug.Log(answer);
+
+        // string str1 = "Hello";
+        // string str2 = "birthay";
+        // string message;
+
+        // message = str1 + str2;
+        // Debug.Log(message);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
